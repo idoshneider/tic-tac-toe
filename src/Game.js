c8db48f206f5121182fe0883c0e5e9b3c279e194
idoshneider <ido.shn@gmail.com>
2022-03-09 14:14:09 +0200

    first commit

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,5 @@
 import React from "react";
-import './game.css'
-// abcd
-import a from '.'
+import './game.css';
 import { useSelector, useDispatch} from "react-redux";
 import store from ".";
 import DrawX from "./actions/DrawX";
