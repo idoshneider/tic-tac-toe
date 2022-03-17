2e8961e7899e33c6993652eff39b585e32ee1362
idoshneider <ido.shn@gmail.com>
2022-03-17 09:19:29 +0200

    added map function

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,15 @@
+import store from ".";
 import React from "react";
 import './game.css';
-import { useSelector, useDispatch} from "react-redux";
-import store from ".";
+import {useSelector, useDispatch} from "react-redux";
+
 import {
  Link
 } from "react-router-dom";
 import { NextSymbol } from "./Logic";
 import { ClearTable } from "./Logic";
 function Game (){
+    const arr = Array(9).fill('');
     const draw = useSelector(state => state.draw) ;   
     const dispatch = useDispatch();
     store.subscribe(() => console.log(store.getState()));
@@ -16,15 +18,9 @@ function Game (){
       <h1 id="gameover">{null}</h1>
       <h3 id="winner">{null}</h3>
       <div className="board">
-        <button className="placeMark" id = "0" onClick={() => NextSymbol("0",dispatch,draw)}></button>
-        <button className="placeMark" id = "1" onClick={() => NextSymbol("1",dispatch,draw)}></button>
-        <button className="placeMark" id = "2" onClick={() => NextSymbol("2",dispatch,draw)}></button>
-        <button className="placeMark" id = "3" onClick={() => NextSymbol("3",dispatch,draw)}></button>
-        <button className="placeMark" id = "4" onClick={() => NextSymbol("4",dispatch,draw)}></button>
-        <button className="placeMark" id = "5" onClick={() => NextSymbol("5",dispatch,draw)}></button>
-        <button className="placeMark" id = "6" onClick={() => NextSymbol("6",dispatch,draw)}></button>
-        <button className="placeMark" id = "7" onClick={() => NextSymbol("7",dispatch,draw)}></button>
-        <button className="placeMark" id = "8" onClick={() => NextSymbol("8",dispatch,draw)}></button>
+        {arr.map((value,index) =>{
+          return <button className="placeMark" id={index} onClick={()=> NextSymbol(index,dispatch,draw)} key ={index}>{value}</button>
+        })}
      </div>
           <div>
             <button className="clear" onClick={() => ClearTable(dispatch)}>Clear</button>
