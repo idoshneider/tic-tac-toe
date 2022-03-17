77d224906e76c5506bea2afe4cf8e5bbae71ea78
idoshneider <ido.shn@gmail.com>
2022-03-17 14:34:58 +0200

    added map function

--- src/Game.js
+++ src/Game.js
@@ -1,26 +1,37 @@
-import store from ".";
 import React from "react";
 import './game.css';
-import {useSelector, useDispatch} from "react-redux";
-
+//a
+import { useSelector, useDispatch} from "react-redux";
+import store from ".";
 import {
  Link
 } from "react-router-dom";
 import { NextSymbol } from "./Logic";
 import { ClearTable } from "./Logic";
 function Game (){
-    const arr = Array(9).fill('');
     const draw = useSelector(state => state.draw) ;   
     const dispatch = useDispatch();
     store.subscribe(() => console.log(store.getState()));
+    var arr = Array(9).fill('');
     return( 
     <div className="game">
       <h1 id="gameover">{null}</h1>
       <h3 id="winner">{null}</h3>
       <div className="board">
-        {arr.map((value,index) =>{
-          return <button className="placeMark" id={index} onClick={()=> NextSymbol(index,dispatch,draw)} key ={index}>{value}</button>
-        })}
+        {/* <button className="placeMark" id = "0" onClick={() => NextSymbol("0",dispatch,draw)}></button>
+        <button className="placeMark" id = "1" onClick={() => NextSymbol("1",dispatch,draw)}></button>
+        <button className="placeMark" id = "2" onClick={() => NextSymbol("2",dispatch,draw)}></button>
+        <button className="placeMark" id = "3" onClick={() => NextSymbol("3",dispatch,draw)}></button>
+        <button className="placeMark" id = "4" onClick={() => NextSymbol("4",dispatch,draw)}></button>
+        <button className="placeMark" id = "5" onClick={() => NextSymbol("5",dispatch,draw)}></button>
+        <button className="placeMark" id = "6" onClick={() => NextSymbol("6",dispatch,draw)}></button>
+        <button className="placeMark" id = "7" onClick={() => NextSymbol("7",dispatch,draw)}></button>
+        <button className="placeMark" id = "8" onClick={() => NextSymbol("8",dispatch,draw)}></button> */}
+        {
+          arr.map((value,index) =>{
+            return <button key={index} id={index} className='placeMark' onClick={() => NextSymbol(index,dispatch,draw)}></button>
+          }
+          )}
      </div>
           <div>
             <button className="clear" onClick={() => ClearTable(dispatch)}>Clear</button>
