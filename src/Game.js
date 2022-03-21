f472e5f8aacfcf2ccefed5744e3458418055e51b
idoshneider <ido.shn@gmail.com>
2022-03-21 15:00:43 +0200

    changes

--- src/Game.js
+++ src/Game.js
@@ -14,29 +14,35 @@ function Game (){
     store.subscribe(() => console.log(store.getState()));
     let arr = Array(9).fill('');
     return( 
-    <div className="game">
-      <h1 id="gameover">{null}</h1>
-      <h3 id="turn">Turn: {draw}</h3>
-      <h3 id="winner">{null}</h3>
-      <div className="board">
-        {
-          arr.map((value,index) =>{
-            return <button key={index} id={index} className='placeMark' onClick={() => NextSymbol(index,dispatch,draw,buttonArr)}>
-                      {buttonArr[index]}
-                   </button>
-          }
-          )}
-     </div>
-          <div>
-            <button className="clear" onClick={() => ClearTable(dispatch,buttonArr)}>
-              Clear
-            </button>
-            <Link to="/">
-              <button className='homeBtn' onClick={() => ClearTable(dispatch,buttonArr)} >
-                Home
+      <div className="game">
+        <h1 id="gameover">
+          {null}
+        </h1>
+        <h3 id="turn">
+          Turn: {draw}
+        </h3>
+        <h3 id="winner">
+          {null}
+        </h3>
+        <div className="board">
+          {
+            arr.map((value,index) =>{
+              return <button key={index} id={index} className='placeMark' onClick={() => NextSymbol(index,dispatch,draw,buttonArr)}>
+                        {buttonArr[index]}
+                    </button>
+            }
+            )}
+      </div>
+            <div>
+              <button className="clear" onClick={() => ClearTable(dispatch,buttonArr)}>
+                Clear
               </button>
-            </Link>
-          </div> 
+              <Link to="/">
+                <button className='homeBtn' onClick={() => ClearTable(dispatch,buttonArr)} >
+                  Home
+                </button>
+              </Link>
+            </div> 
     </div>
     )
   }
